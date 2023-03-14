a1199f7a53acc512a3fba3af479599207547d9f7
zero-id <abdullatiffebriliana15@gmail.com>
2023-03-14 21:00:36 +0700

    tugas regExp bagian 1 js

---
+++ tugas-regExp.js
@@ -0,0 +1,11 @@
+
+function panggilRegexp(value) {
+ let str = "abcdefghijklmnopqrstuvwxyz-A        B        C        D        E        F        G        H        I        J        K        L        M        N        O        P        Q        R        S        T        U        V        W        X        Y        Z";
+
+ //Tulis Code Jawaban Kamu Di Bawah ini
+    console.log(str.match(/c/ig));
+    console.log(str.match(/k/ig));
+    console.log(str.match(/l/ig));
+}
+
+panggilRegexp()
